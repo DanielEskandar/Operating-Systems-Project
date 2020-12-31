52cb1ee47c256fc9a1e768b959e59acc9acd351f
Daniel Eskandar <danieleskandar@yahoo.com>
2020-12-31 09:06:50 +0200

    Added enqueue function and generation main loop

--- Scheduler/Code/process_generator.c
+++ Scheduler/Code/process_generator.c
@@ -32,26 +32,36 @@ int main(int argc, char * argv[])
 		}
 	}   
 	fclose(pFile);
+	printf("N = %d\n", N);
 
 	// read input file and create an array of processes
 	pFile = fopen(argv[1], "r");
-	struct process *processArray = (struct process *)malloc(N * sizeof(struct process));
+	struct process *processArray = (struct process *) malloc(N * sizeof(struct process));
 	fscanf(pFile, "%*[^\n]\n"); 
 	for (int i = 0; i < N; i++)
 	{
 		fscanf(pFile, "%d\t%d\t%d\t%d\n", &processArray[i].id, &processArray[i].arrivalTime, &processArray[i].runningTime, &processArray[i].priority);
+		processArray[i].remainingTime = processArray[i].runningTime;
 		processArray[i].next = NULL;
 		processArray[i].prev = NULL;
 	}
 	fclose(pFile);
 	
-	printf("debug print\n");
+	// create sempahore between scheduler and generator
+	int scheduler_sem = semget(SCHEDULER_SEM_KEY, 1, IPC_CREAT | 0644);
+	union Semun semun;
+	semun.val = 0;
+	if (semctl(scheduler_sem, 0, SETVAL, semun) == -1)
+	{
+		perror("Error in semctl\n");
+		exit(-1);
+	}
 	
 	// create shared memory between scheduler and generator
-	int scheduler_shmid = shmget(SCHEDULER_KEY, (sizeof(struct schedulerInfo) + sizeof(struct readyQueue) + (N * sizeof(struct process))), IPC_CREAT | 0644);
+	int scheduler_shmid = shmget(SCHEDULER_SHM_KEY, (sizeof(struct schedulerInfo) + sizeof(struct readyQueue) + (N * sizeof(struct process))), IPC_CREAT | 0644);
+	printf("Shared memory size = %ld\n", (sizeof(struct schedulerInfo) + sizeof(struct readyQueue) + (N * sizeof(struct process)))); 
 	struct schedulerInfo *p_schedulerInfo = (struct schedulerInfo *) shmat(scheduler_shmid, (void *)0, 0);
-	struct readyQueue *p_readyQueue = (struct readyQueue *) (p_schedulerInfo + sizeof(struct schedulerInfo));
-	
+	struct readyQueue *p_readyQueue = (struct readyQueue *) (p_schedulerInfo + 1);
 	// initialize shared memory
 	p_schedulerInfo->generationFinished = false;
 	p_schedulerInfo->quantum = 0;
@@ -79,24 +89,55 @@ int main(int argc, char * argv[])
 
 	// initialize clock
 	initClk();
-
-	// To get time use this
-	int x = getClk();
-	printf("current time is %d\n", x);
+	int currentTime = getClk();
+	printf("Current time is %d\n", currentTime);
 	
-	// TODO Generation Main Loop
-	// 5. Create a data structure for processes and provide it with its parameters.
-	// 6. Send the information to the scheduler at the appropriate time.
+	// generation main loop
+	int processIndex = 0;
+	struct process *p_process = (struct process *) (p_readyQueue + 1);
+	while (!p_schedulerInfo->generationFinished)
+	{
+		// send the information to the scheduler in its appropriate time
+		while (processArray[processIndex].arrivalTime == currentTime)
+		{			 
+			*p_process = processArray[processIndex]; // physical allocation
+			enqueue(p_readyQueue, p_process, p_schedulerInfo->schedulerType);
+			
+			if ((processIndex + 1) == N)
+			{
+				p_schedulerInfo->generationFinished = true;
+				break;
+			}
+			p_process += 1;
+			processIndex++;
+		}
+					
+		// enable scheduler to operate on the ready queue
+		// up(scheduler_sem);
+		
+		// wait until clk changes
+		while (currentTime == getClk());
+		currentTime = getClk();
+	}
+	
+	struct process *current = p_readyQueue->head;
+	while (current != NULL)
+	{
+		printf("%d\t%d\t%d\t%d\n", current->id, current->arrivalTime, current->runningTime, current->priority);
+		current = current->next;
+	}
 	
 	// Wait until scheduler finishes
-	while (1) ;
+	while (1);
 }
 
 void clearResources(int signum)
 {
 	//TODO Clears all resources in case of interruption
 	
+	// free	processArray
 	// clear shared memroy between generator and scheduler
+	// clear semaphore between generator and scheduler
 
 	// clear clk resources
 	destroyClk(true);
--- Scheduler/Code/scheduler_utilities.h
+++ Scheduler/Code/scheduler_utilities.h
@@ -1,14 +1,16 @@
 #define HPF 0
 #define SRTN 1
 #define RR 2
-#define SCHEDULER_KEY 500
+#define SCHEDULER_SHM_KEY 500
+#define SCHEDULER_SEM_KEY 600
 
 struct process
 {
-	int id;
+	int id;	
 	int arrivalTime;
 	int runningTime;
 	int priority;
+	int remainingTime;
 	struct process* next;
 	struct process* prev;
 };
@@ -28,3 +30,93 @@ struct readyQueue
 	bool processArrival;
 };
 
+void enqueue(struct readyQueue *p_readyQueue, struct process *p_process, int schedulerType)
+{	
+	// corner case: empty queue
+	if (p_readyQueue->size == 0)
+	{
+		p_readyQueue->head = p_process;
+		p_readyQueue->tail = p_process;
+		p_readyQueue->size++;
+		return;
+	}
+	
+	struct process *current = p_readyQueue->head;
+	switch (schedulerType)
+	{
+		case HPF:
+			// corner case: process has highest priority
+			if (p_process->priority < p_readyQueue->head->priority)
+			{
+				p_process->next = p_readyQueue->head;
+				p_readyQueue->head->prev = p_process;
+				p_readyQueue->head = p_process;
+				p_readyQueue->size++;
+				return;
+			}
+			
+			while (current->next != NULL)
+			{
+				if (p_process->priority < current->next->priority)
+				{
+					p_process->next = current->next;
+					p_process->prev = current;
+					current->next->prev = p_process;
+					current->next = p_process;
+					p_readyQueue->size++;
+					return;
+				}
+				current = current->next;
+			}
+			
+			// corner case: process has lowest priority
+			current->next = p_process;
+			p_process->prev = current;
+			p_readyQueue->tail = p_process;
+			p_readyQueue->size++;
+			return;
+		
+		case SRTN:
+			// corner case: process has lowest remaining time
+			if (p_process->remainingTime < p_readyQueue->head->remainingTime)
+			{
+				p_process->next = p_readyQueue->head;
+				p_readyQueue->head->prev = p_process;
+				p_readyQueue->head = p_process;
+				p_readyQueue->size++;
+				return;
+			}
+			
+			while (current->next != NULL)
+			{
+				if (p_process->remainingTime < current->next->remainingTime)
+				{
+					p_process->next = current->next;
+					p_process->prev = current;
+					current->next->prev = p_process;
+					current->next = p_process;
+					p_readyQueue->size++;
+					return;
+				}
+				current = current->next;
+			}
+			
+			// corner case: process has highest remaining time
+			current->next = p_process;
+			p_process->prev = current;
+			p_readyQueue->tail = p_process;
+			p_readyQueue->size++;
+			return;
+			
+		case RR:
+			while (current->next != NULL)
+			{
+				current = current->next;
+			}
+			current->next = p_process;
+			p_process->prev = current;
+			p_readyQueue->tail = p_process;
+			p_readyQueue->size++;
+			return;
+	}
+}
